5c4e3098621f35bbc9e5ac3747c828bd501a7b93
Fabio Silva <fabinhoevil16@hotmail.com>
2026-03-02 16:44:42 -0300

    alterando o codigo para OO

---
+++ Classes,atrib,metodos/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Globalization;
+
+namespace Course 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);        
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = x.Area();   
+
+            double q = (y.A + y.B + y.C) / 2.0;
+            double areaY = y.Area();   
+
+            Console.WriteLine("Área do triângulo X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área do triângulo Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            
+            if (areaX > areaY) 
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else 
+            {
+                Console.WriteLine("Maior área: Y");
+            } 
+        }
+    }
+}
