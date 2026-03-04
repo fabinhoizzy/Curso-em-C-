f1f9c565e3cf62720cd1e147fd2b1102d29a5f34
Fabio Silva <fabinhoevil16@hotmail.com>
2026-03-04 14:28:37 -0300

    membros static

---
+++ membros/Calculadora.cs
@@ -0,0 +1,19 @@
+
+namespace Course {
+    public class Calculadora
+    {
+
+        public static double PI = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * PI * r;
+        }  
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * PI * r * r * r ;
+        }
+    }           
+}
+    
