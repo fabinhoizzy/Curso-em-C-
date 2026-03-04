3880f60a010a998be9d26db8c50bb03d462e22d8
Fabio Silva <fabinhoevil16@hotmail.com>
2026-03-04 11:02:48 -0300

    Problema de estoque

---
+++ exercicio/Program.cs
@@ -0,0 +1,43 @@
+
+using System.Globalization;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizado: " + p);
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizado: " + p);
+
+        }
+    }
+}
